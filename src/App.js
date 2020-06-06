7af7e047d2a963a6358329387b1ca6055788bc46
zeroproduct <danrica92@yahoo.com>
2020-06-05 20:27:31 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const re = /\S+@\S+\.\S+/;
+
 function App() {
+  const [recipient, setRecipient] = useState('');
+  const [recipientError, setRecipientError] = useState(null);
+  const [subject, setSubject] = useState('');
+  const [body, setBody] = useState('');
+  const [buttonText, setButtonText] = useState('Generate Link');
+  const [generatedMailto, setGeneratedMailto] = useState('');
+
+  const generateLink = () => {
+    if (recipient === '' || subject === '') {
+      setButtonText('Please enter text in blank fields');
+    } else if (recipientError) {
+      setButtonText('Please enter a valid email format');
+    } else {
+      let mailtoLink = 'mailto:';
+      mailtoLink += recipient + '&subject=' + subject.split(' ').join('%20');
+      let bodyStr = JSON.stringify(body);
+      bodyStr = bodyStr.split("\"").join("");
+      bodyStr = bodyStr.split("\\n").join("%0A");
+      mailtoLink += '&body=' + bodyStr;
+  
+      setGeneratedMailto(mailtoLink);
+    }
+  };
+
+  const handleEmailInput = e => {
+    setRecipient(e.target.value);
+    
+    if (re.test(e.target.value)) {
+      setButtonText('Generate Link');
+      setRecipientError(false);
+    } else {
+      setRecipientError(true);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="title">
+        <div style={{ display: 'inline-block'}} className="logo">
+          <div style={{ display: 'flex'}}>
+            <div style={{ height: '10px', width: '10px', backgroundColor: '#063940'}} />
+            <div style={{ height: '10px', width: '10px', backgroundColor: '#195E63'}} />
+          </div>
+          <div style={{ display: 'flex'}}>
+            <div style={{ height: '10px', width: '10px', backgroundColor: '#3E838C'}} />
+            <div style={{ height: '10px', width: '10px', backgroundColor: '#8EBDB6'}} />
+          </div>
+        </div>
+        <span style={{ marginLeft: '12px' }}>MAIL-TO</span>
+      </div>
+      <div className="email-header">
+        <div className="email-header-line">
+          <label>To: </label>
+          <input 
+            type="text" 
+            style= {{ color: recipientError ? '#ff0000b3' : '#000000' }}
+            value={recipient} 
+            onChange={handleEmailInput} />
+        </div>
+        <div className="email-header-line">
+          <label>Subject: </label>
+          <input type="text" value={subject} onChange={e => setSubject(e.target.value)} />
+        </div>
+      </div>
+      <div className="email-body">
+        <label>Body: </label>
+        <textarea value={body} onChange={e => setBody(e.target.value)} />
+        <input style={{ visibility: 'visible' }} type="text" defaultValue={generatedMailto} />
+        <button onClick={() => generateLink()}>{buttonText}</button>
+      </div>
     </div>
   );
 }
